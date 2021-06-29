18001cc34789a42099046fcff6834ae229bce0a2
Alex Moreno <alex.costa@picpay.com>
2021-06-29 11:25:25 -0300

    aula-4

--- hello.go
+++ hello.go
@@ -1,12 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
+	start()
+
+	for {
+		option := selectedOption()
+
+		switch option {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Opção não reconhecida!!")
+			os.Exit(-1)
+		}
+	}
+}
+
+func start() {
 	nome := "Monitor de sites"
 	versao := 1.1
-
 	fmt.Println("Iniciando o programa", nome, "versão", versao)
+}
+
+func selectedOption() int {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair")
@@ -14,5 +41,18 @@ func main() {
 	var opcao int
 	fmt.Scan(&opcao)
 
-	fmt.Println("A opção escolhida foi", opcao)
+	return opcao
+}
+
+func startMonitoring() {
+	fmt.Println("Monitorando...")
+
+	url := "https://www.alura.com.br"
+	response, _ := http.Get(url)
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", url, "Ok!")
+	} else {
+		fmt.Println("Site:", url, "Falhou!")
+	}
 }
