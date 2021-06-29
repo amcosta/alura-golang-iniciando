c2a671aa717b3cf4ef5572582126ebe692cbbe84
Alex Moreno <alex.costa@picpay.com>
2021-06-28 22:07:50 -0300

    aula-2

--- hello.go
+++ hello.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá mundo")
+	nome := "Monitor de sites"
+	versao := 1.1
+
+	fmt.Println("Iniciando o programa", nome, "versão", versao)
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair")
+
+	var opcao int
+	fmt.Scan(&opcao)
+
+	fmt.Println("A opção escolhida foi", opcao)
 }
